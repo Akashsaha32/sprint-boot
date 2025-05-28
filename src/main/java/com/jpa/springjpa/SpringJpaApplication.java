1a58ffb745a42900721aa3544a16adb083bd6464
Akashsaha32 <saha15-5764@diu.edu.bd>
2025-05-28 16:52:37 +0600

    initialize spring project

---
+++ src/main/java/com/jpa/springjpa/SpringJpaApplication.java
@@ -0,0 +1,10 @@
+package com.jpa.springjpa;
+
+
+public class SpringJpaApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Spring JPA Application Started");
+    }
+
+}
