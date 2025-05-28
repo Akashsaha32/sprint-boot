d469a78d10d964b1f5e93b8206808ee835506d42
Akashsaha32 <saha15-5764@diu.edu.bd>
2025-05-28 17:06:57 +0600

    finish office

--- src/main/java/com/jpa/springjpa/SpringJpaApplication.java
+++ src/main/java/com/jpa/springjpa/SpringJpaApplication.java
@@ -1,10 +1,14 @@
 package com.jpa.springjpa;
 
 
+import jakarta.persistence.EntityManager;
+
 public class SpringJpaApplication {
 
     public static void main(String[] args) {
         System.out.println("Spring JPA Application Started");
+
+        EntityManager em;
     }
 
 }
