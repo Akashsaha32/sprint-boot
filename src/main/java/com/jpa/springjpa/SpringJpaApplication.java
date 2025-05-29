27e1eb5a5f0a691049b7b17940dfc616594c6623
Akashsaha32 <saha15-5764@diu.edu.bd>
2025-05-29 16:14:55 +0600

    from screts class 55

--- src/main/java/com/jpa/springjpa/SpringJpaApplication.java
+++ src/main/java/com/jpa/springjpa/SpringJpaApplication.java
@@ -1,24 +1,8 @@
 package com.jpa.springjpa;
 
 
-import com.jpa.springjpa.dao.StudentDAO;
-import com.jpa.springjpa.entity.Student;
-import jakarta.persistence.EntityManager;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class SpringJpaApplication implements CommandLineRunner {
-
-    @Autowired
-    private EntityManager em;
+public class SpringJpaApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringJpaApplication.class, args);
-    }
 
-    public void run(String... args) throws Exception {
-        System.out.println(em.find(Student.class, 1));
     }
 }
