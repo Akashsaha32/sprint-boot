fad7f836766713112de435d10ae9db53dfba1d42
Akashsaha32 <saha15-5764@diu.edu.bd>
2025-05-29 16:10:32 +0600

    Class 55 start, Using EntityManager

--- src/main/java/com/jpa/springjpa/SpringJpaApplication.java
+++ src/main/java/com/jpa/springjpa/SpringJpaApplication.java
@@ -3,6 +3,7 @@ package com.jpa.springjpa;
 
 import com.jpa.springjpa.dao.StudentDAO;
 import com.jpa.springjpa.entity.Student;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,13 +13,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringJpaApplication implements CommandLineRunner {
 
     @Autowired
-    private StudentDAO studentDAO;
+    private EntityManager em;
     public static void main(String[] args) {
         SpringApplication.run(SpringJpaApplication.class, args);
     }
 
     public void run(String... args) throws Exception {
-        studentDAO.save(new Student(11, "Jack Fabric", 3.20));
-        studentDAO.findAll().forEach(System.out::println);
+        System.out.println(em.find(Student.class, 1));
     }
 }
