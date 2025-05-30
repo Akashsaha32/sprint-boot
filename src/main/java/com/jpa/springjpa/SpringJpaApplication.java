763ff605c5bae97ab2c4118cc7cb0e221e95e0f7
Akashsaha32 <saha15-5764@diu.edu.bd>
2025-05-30 16:53:55 +0600

    transaction creation

--- src/main/java/com/jpa/springjpa/SpringJpaApplication.java
+++ src/main/java/com/jpa/springjpa/SpringJpaApplication.java
@@ -1,6 +1,7 @@
 package com.jpa.springjpa;
 
 
+import com.jpa.springjpa.entity.Student;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -9,6 +10,13 @@ public class SpringJpaApplication {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("test_01");
         EntityManager em = emf.createEntityManager();
-        System.out.println(em);
+
+        em.getTransaction().begin(); //every entitymanager creates only one transaction
+        Student s = new Student();
+        s.setName("Jack Tharmost");
+        s.setCgpa(3.77);
+
+        em.persist(s); // just cash data
+        em.getTransaction().commit(); // reflect to db
     }
 }
