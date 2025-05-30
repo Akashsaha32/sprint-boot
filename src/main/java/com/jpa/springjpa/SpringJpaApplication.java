412fa1ac3c4bbd93a38c66d39689dbef33190659
Akashsaha32 <saha15-5764@diu.edu.bd>
2025-05-30 16:04:21 +0600

    making persistence unit in xml, META-INF, persistence.xml
    EntityManagerFactory, EntityManager

--- src/main/java/com/jpa/springjpa/SpringJpaApplication.java
+++ src/main/java/com/jpa/springjpa/SpringJpaApplication.java
@@ -1,8 +1,14 @@
 package com.jpa.springjpa;
 
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class SpringJpaApplication {
     public static void main(String[] args) {
-
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("test_01");
+        EntityManager em = emf.createEntityManager();
+        System.out.println(em);
     }
 }
