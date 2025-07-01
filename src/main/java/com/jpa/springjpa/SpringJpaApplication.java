0782276736a366f5e6d63c1cab8b65b83cb3a05d
Akashsaha32 <saha15-5764@diu.edu.bd>
2025-07-01 14:45:48 +0600

    remove all comment and fresh start class 57

--- src/main/java/com/jpa/springjpa/SpringJpaApplication.java
+++ src/main/java/com/jpa/springjpa/SpringJpaApplication.java
@@ -16,51 +16,8 @@ public class SpringJpaApplication {
     private final static EntityManager em = emf.createEntityManager();
     public static void main(String[] args) {
 
-        /*try {
-            em.getTransaction().begin(); //every entitymanager creates only one transaction
-            Student s = new Student();
-            s.setName("Jack Tharmost");
-            s.setCgpa(3.77);
-            em.persist(s); // just cash data
-
-            double ans = 1/0;
-
-            em.getTransaction().commit(); // reflect to db
-        }catch (Exception e) {
-            e.printStackTrace();
-            em.getTransaction().rollback();
-        }*/
-
-        /*
-        * 1. Transient State
-        * 2. Persistence State / Managed State
-        * 3. detach state
-        * 4. remove state
-        */
         transactional((em) -> {
-            /*Student s = new Student(null, "Mark Beniof", 3.98);
 
-            em.persist(s);
-            s.setCgpa(2.50);
-            System.out.println(s);
-            em.detach(s);*/
-
-            var map = em.getProperties();
-            map.forEach((k, v) -> {
-                System.out.println(k + " => "+v);
-            });
-
-            var stu = em.find(Student.class, 21);
-            em.detach(stu);
-            stu.setCgpa(4.00);
-            em.merge(stu); //if id get then update if not then create (first select -> then insert or update)
-            stu.setCgpa(5.00); // so it will not reflect in DB.
-
-            /*
-            * If data is in manage state then don't need save, update or merge
-            * after successfully commit it will automatically happen
-            * don't wast unnecessary cpu burn
-            */
         });
     }
 
