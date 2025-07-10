a87d8af7bdc1d33400955086b512543c1c42ce04
Akashsaha32 <saha15-5764@diu.edu.bd>
2025-07-10 13:47:42 +0600

    JoinTable is not needed in one to many, or one to one or many-to-one relationship
    class 60 complete

--- src/main/java/com/jpa/springjpa/onotoone/Student_Relationship.java
+++ src/main/java/com/jpa/springjpa/onotoone/Student_Relationship.java
@@ -21,8 +21,15 @@ public class Student_Relationship {
         Here this constrain is add with this name
         Foreign Key Constrain is if Student exist then Address of that Student can not be deleted
         If we don't want constrain then can set "ConstraintMode.NO_CONSTRAINT"
+
+        if we want foreign key is generate a separate table we can use @JoinTable instead of @JoinColumn
      */
     @OneToOne
-    @JoinColumn(name = "address_identifier", referencedColumnName = "aid")
+    //@JoinColumn(name = "address_identifier", referencedColumnName = "aid")
+    @JoinTable(
+        name = "stu_add",
+            joinColumns = @JoinColumn(name = "stu_id"),
+            inverseJoinColumns = @JoinColumn(name = "add_id")
+    )
     private Address address;
 }
