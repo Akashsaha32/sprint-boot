21108d138a9615f4aeac54a708d8933a09b85244
Akashsaha32 <saha15-5764@diu.edu.bd>
2025-07-10 10:42:49 +0600

    class 60 start

--- src/main/java/com/jpa/springjpa/SpringJpaApplication.java
+++ src/main/java/com/jpa/springjpa/SpringJpaApplication.java
@@ -27,63 +27,7 @@ public class SpringJpaApplication {
         );
 
         transactional((em) -> {
-            // need to customization or category wise search
-            /*CriteriaBuilder cb = em.getCriteriaBuilder();
-            CriteriaQuery<Student> cq = cb.createQuery(Student.class);
-            Root<Student> root = cq.from(Student.class);
-            root.fetch("courses", JoinType.LEFT);
-            cq.where(cb.equal(root.get("name"), "Akash Saha"));
-            //cq.where(cb.equal(root.get("courses").get("code"), "CSE-231"));
 
-            TypedQuery<Student> query = em.createQuery(cq);
-            query.getResultStream().forEach(System.out::println);
-             */
-
-
-            /*CriteriaBuilder cb = em.getCriteriaBuilder();
-            //CriteriaQuery<String> cq = cb.createQuery(String.class);
-            CriteriaQuery<Object[]> cq = cb.createQuery(Object[].class);
-            Root<Student> root = cq.from(Student.class);
-            //root.fetch("courses", JoinType.LEFT);
-            cq.multiselect(root.get("name"), root.get("cgpa"));
-            cq.where(cb.equal(root.get("id"), 1));
-            //cq.where(cb.equal(root.get("name"), "Akash Saha"));
-            //cq.where(cb.equal(root.get("courses").get("code"), "CSE-231"));
-
-            TypedQuery<Object[]> query = em.createQuery(cq);
-            query.getResultStream().forEach((a) -> {
-                System.out.println(a[0]+", "+a[1]);
-            });*/
-
-            CriteriaBuilder cb = em.getCriteriaBuilder();
-            CriteriaQuery<Product> cq = cb.createQuery(Product.class);
-            Root<Product> root = cq.from(Product.class);
-
-            ArrayList<Predicate> list = new ArrayList<Predicate>();
-
-            if(filter.searchValue() != null && !filter.searchValue().isEmpty()) {
-                String value = filter.searchValue();
-                Predicate np = cb.like(root.get("name"), "%" + value + "%");
-                var  dp = cb.like(root.get("description"), "%" + value + "%");
-                list.add(cb.or(np, dp));
-            }
-
-            if(filter.category() != null) {
-                list.add(cb.equal(root.get("category"), filter.category()));
-            }
-
-            if(filter.min_price() > 0 && filter.max_price() > 0 && filter.min_price() < filter.max_price()) {
-                list.add(cb.between(root.get("price"), filter.min_price(), filter.max_price()));
-            }
-
-            if(filter.brand() != null) {
-                Predicate p = root.get("brand").in(filter.brand());
-                list.add(p);
-            }
-
-            cq.where(list.toArray(new Predicate[0]));
-            var q = em.createQuery(cq);
-            q.getResultStream().forEach(System.out::println);
         });
     }
 
